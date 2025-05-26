acd6e307afc2575179fff66db9aaa08af61e3308
Jorge Gregorio Donaire Pereyra <jorgedonaire93@gmail.com>
2025-05-26 00:35:54 -0300

    segundo commit en calculadorav2

--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
 ﻿//CalculadoraV2
 Console.WriteLine("***** Calculadora V2 ******");
+Console.WriteLine();
+Console.WriteLine("Ingrese un numero: ");
 
 Console.WriteLine();
 Console.WriteLine("****** Calculadora Finalizada ******");
